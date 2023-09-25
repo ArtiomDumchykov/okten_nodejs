9c2a6ec0b127a46169399a7a6a0e8344ed20f7b4
ArtiomDumchykov <66011687+ArtiomDumchykov@users.noreply.github.com>
2023-09-25 17:04:13 +0300

    fix

---
+++ routes/home.js
@@ -0,0 +1,9 @@
+const {Router} = require("express")
+
+const router = Router()
+
+router.get("/", async(req,res) => {
+    res.status(200).send("<h1>Hello World</h1>");
+})
+
+module.exports = router
