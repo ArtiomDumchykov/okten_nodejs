eb4a888f25959d5b8a1407fb554f3e850ce88484
ArtiomDumchykov <66011687+ArtiomDumchykov@users.noreply.github.com>
2023-09-28 01:37:12 +0300

    fix

---
+++ src/types/userstype.ts
@@ -0,0 +1,12 @@
+import { Document } from "mongoose";
+
+import { EGenders } from "../enums";
+
+
+export interface IUser extends Document {
+    name: string
+    age?: string
+    genders?: EGenders
+    email: string,
+    password: string
+}
