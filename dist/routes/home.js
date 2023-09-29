a9156e69e709a2dc3542842df41062fc8ccbf7f0
ArtiomDumchykov <66011687+ArtiomDumchykov@users.noreply.github.com>
2023-09-29 19:57:42 +0300

    fix

--- dist/routes/home.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.homeRoutes = void 0;
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get("/", async (req, res) => {
-    res.status(200).send("<h1>Hello World</h1>");
-});
-exports.homeRoutes = router;
