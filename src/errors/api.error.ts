880a997b9a965b48421d8913668fb8351300f7ca
ArtiomDumchykov <66011687+ArtiomDumchykov@users.noreply.github.com>
2023-10-14 14:25:27 +0300

    fix

--- src/errors/api.error.ts
+++ src/errors/api.error.ts
@@ -13,18 +13,3 @@ export class ApiError extends Error {
     ApiError.instance = this;
   }
 }
-// export class ApiError extends Error {
-//   static instance: any;
-//   public status: number;
-
-//   constructor(message: string, status: number) {
-//     super(message);
-//     this.status = status;
-
-//     if (typeof ApiError.instance === "object") {
-//       return ApiError.instance;
-//     }
-
-//     ApiError.instance = this;
-//   }
-// }
